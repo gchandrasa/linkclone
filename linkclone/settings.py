abbc69baa94aa1cf7ee07c305ea5e6a1de86dd44
Gilang Chandrasa <gilang.chandrasa@gmail.com>
2013-12-26 06:57:46 +0700

    Update requirements.txt

--- linkclone/settings.py
+++ linkclone/settings.py
@@ -97,3 +97,11 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates')
 )
 
+# local_settings.py can be used to override environment-specific settings
+# like database and email that differ between development and production.
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
